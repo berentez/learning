a0ca4cb02d7315224d1ff6101a5ac00101662946
Zoltan Berente <berentez07b@gmail.com>
2021-05-30 16:53:19 +0200

    finished CA

--- react/CA/FavQs/quotes/src/components/quoteList/quoteList.js
+++ react/CA/FavQs/quotes/src/components/quoteList/quoteList.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Pagination } from 'antd';
 
 import Qotd from '../qotd/qotd';
 
 import './quoteList.css';
 
 const QuoteList = ({ quotes }) => {
+  const [page, setPage] = useState(0);
+  const pageSize = 6;
+
+  const onPaginationChange = (page, pageSize) => {
+    setPage(page - 1);
+  };
+
   return (
     <div className="quote-list">
-      {quotes.map((quote) => (
+      {quotes.slice(page * pageSize, (page + 1) * pageSize).map((quote) => (
         <Qotd key={quote.id} quote={quote.body} author={quote.author} />
       ))}
+      <Pagination current={page + 1} total={quotes.length} onChange={onPaginationChange} pageSize={pageSize} />
     </div>
   );
 };
