8dbc1b9268e6ff2efb846ca1f97f72d4d9615885
Zoltan Berente <berentez07b@gmail.com>
2021-11-22 20:08:13 +0100

    footer added

--- tutorial/pizza-site/src/App.js
+++ tutorial/pizza-site/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
