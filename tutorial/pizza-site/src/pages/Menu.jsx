b25183ca2dc9a2da32de1be6b259b14d423232d1
Zoltan Berente <berentez07b@gmail.com>
2021-11-23 20:10:34 +0100

    Menu style added

--- tutorial/pizza-site/src/pages/Menu.jsx
+++ tutorial/pizza-site/src/pages/Menu.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { MenuList } from '../helpers/MenuList';
 import MenuItem from '../components/MenuItem';
 
+import '../styles/Menu.css';
+
 function Menu() {
   return (
     <div className="menu">
