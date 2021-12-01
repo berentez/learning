5a4722880dc294316ee900a7f4c85132011cc999
Zoltan Berente <berentez07b@gmail.com>
2021-12-01 19:34:09 +0100

    day-1 part 2 finished

--- advent-of-code/2021/01/depthMeasurement.ts
+++ advent-of-code/2021/01/depthMeasurement.ts
@@ -20,3 +20,27 @@ function readFile(content: string): string {
 }
 
 console.log(countDepthIncrease());
+
+//part2
+
+function countDepthSumIncrease(): number {
+  let text: string = readFile('depths.txt');
+  let textArray: string[] = text.split(/\r?\n/);
+  let increase: number = 0;
+
+  const numArr: number[] = textArray.map(value => {
+    return parseInt(value);
+  });
+
+  for (let i: number = 0; i < numArr.length - 3; i++) {
+    const sum1: number = numArr[i] + numArr[i + 1] + numArr[i + 2];
+    const sum2: number = numArr[i + 1] + numArr[i + 2] + numArr[i + 3];
+
+    if (sum1 < sum2) {
+      increase++;
+    }
+  }
+  return increase;
+}
+
+console.log(countDepthSumIncrease());
