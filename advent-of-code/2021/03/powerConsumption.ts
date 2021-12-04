aae4d1f30158de31ac06f751d1c68e8fd59e526d
Zoltan Berente <berentez07b@gmail.com>
2021-12-04 11:44:18 +0100

    part 1 finished

--- advent-of-code/2021/03/powerConsumption.ts
+++ advent-of-code/2021/03/powerConsumption.ts
@@ -1,7 +1,7 @@
 import { getInput } from '../../getInput';
 import { KeyInterface } from '../../KeyInterface';
 
-const organiseData = (data: string[]): Object => {
+const organiseData = (data: string[]): KeyInterface => {
   let dataObj: KeyInterface = {};
 
   for (let i: number = 0; i < data.length; i++) {
@@ -29,13 +29,37 @@ const transferBinary = (binary: string): number => {
   return res;
 };
 
+interface Powers {
+  gamma: string;
+  epsilon: string;
+}
+
+const buildUpBinary = (obj: KeyInterface, length: number): Powers => {
+  let gammaArr: string[] = [];
+  let epsilonArr: string[] = [];
+  let powers: Powers = { gamma: '', epsilon: '' };
+
+  for (let i: number = 0; i < Object.keys(obj).length; i++) {
+    let value: number = obj[i];
+    if (value > length / 2) {
+      gammaArr.push('1');
+      epsilonArr.push('0');
+    } else {
+      gammaArr.push('0');
+      epsilonArr.push('1');
+    }
+  }
+
+  powers.gamma = gammaArr.join('');
+  powers.epsilon = epsilonArr.join('');
+  return powers;
+};
+
 const calculatePowerConsumption = (): number => {
-  const data = getInput('input.txt', /\r?\n/);
+  const data: string[] = getInput('input.txt', /\r?\n/);
   const dataObj = organiseData(data);
-  console.log(dataObj);
-  //getting the two number
-  //transfering to decimal and calculate power
-  return 0;
+  const powers: Powers = buildUpBinary(dataObj, data.length);
+  return transferBinary(powers.gamma) * transferBinary(powers.epsilon);
 };
 
 console.log(calculatePowerConsumption());
