ba9f2e6c08eec8335340beec2556782281f2073f
Zoltan Berente <berentez07b@gmail.com>
2021-12-04 11:17:34 +0100

    data organised into an object

--- advent-of-code/2021/03/powerConsumption.ts
+++ advent-of-code/2021/03/powerConsumption.ts
@@ -1,11 +1,20 @@
 import { getInput } from '../../getInput';
+import { KeyInterface } from '../../KeyInterface';
 
-const calculatePowerConsumption = (): number => {
-  const data = getInput('input.txt', /\r?\n/);
-  //building up object
-  //getting the two number
-  //transfering to decimal and calculate power
-  return 0;
+const organiseData = (data: string[]): Object => {
+  let dataObj: KeyInterface = {};
+
+  for (let i: number = 0; i < data.length; i++) {
+    for (let n: number = 0; n < data[i].length; n++) {
+      if (!dataObj[n]) {
+        dataObj[n] = 0;
+      }
+      if (data[i][n] === '1') {
+        dataObj[n]++;
+      }
+    }
+  }
+  return dataObj;
 };
 
 const transferBinary = (binary: string): number => {
@@ -19,3 +28,14 @@ const transferBinary = (binary: string): number => {
 
   return res;
 };
+
+const calculatePowerConsumption = (): number => {
+  const data = getInput('input.txt', /\r?\n/);
+  const dataObj = organiseData(data);
+  console.log(dataObj);
+  //getting the two number
+  //transfering to decimal and calculate power
+  return 0;
+};
+
+console.log(calculatePowerConsumption());
