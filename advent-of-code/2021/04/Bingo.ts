eddd1a61298836f451d3d275e1a8e06e38a1d858
Zoltan Berente <berentez07b@gmail.com>
2021-12-05 17:47:08 +0100

    bingo

--- advent-of-code/2021/04/Bingo.ts
+++ advent-of-code/2021/04/Bingo.ts
@@ -1,10 +1,14 @@
-export class BingoSheet {
-  numbers: BingoLine[];
-  win: boolean;
+export class BingoNumber {
+  value: number;
+  marked: boolean;
 
-  constructor(num: BingoLine[]) {
-    this.numbers = num;
-    this.win = false;
+  constructor(num: number) {
+    this.value = num;
+    this.marked = false;
+  }
+
+  markNumber() {
+    this.marked = true;
   }
 }
 
@@ -16,12 +20,60 @@ export class BingoLine {
   }
 }
 
-export class BingoNumber {
-  value: number;
-  marked: boolean;
+export class BingoSheet {
+  numbers: BingoLine[];
+  win: boolean;
 
-  constructor(num: number) {
-    this.value = num;
-    this.marked = false;
+  constructor(num: BingoLine[]) {
+    this.numbers = num;
+    this.win = false;
+  }
+
+  winGame() {
+    this.win = true;
+    console.log('BINGO!!');
+  }
+
+  markNum(mark: number): BingoLine[] {
+    for (let i: number = 0; i < this.numbers.length; i++) {
+      for (let n: number = 0; n < this.numbers[i].numbers.length; n++) {
+        if (mark === this.numbers[i].numbers[n].value) {
+          this.numbers[i].numbers[n].markNumber();
+          this.checkLine(this.numbers[i]);
+          this.checkRow(n);
+
+          if (this.win) {
+            return this.numbers;
+          }
+        }
+      }
+    }
+    return [];
+  }
+
+  checkLine(line: BingoLine) {
+    for (let i: number = 0; i < line.numbers.length; i++) {
+      if (line.numbers[i].marked) {
+        if (i === 4) {
+          this.winGame();
+          console.log('win line');
+        }
+      } else {
+        break;
+      }
+    }
+  }
+
+  checkRow(row: number) {
+    for (let i: number = 0; i < this.numbers.length; i++) {
+      if (this.numbers[i].numbers[row].marked) {
+        if (i === 4) {
+          this.winGame();
+          console.log('win row');
+        }
+      } else {
+        break;
+      }
+    }
   }
 }
--- advent-of-code/2021/04/bingoScore.ts
+++ advent-of-code/2021/04/bingoScore.ts
@@ -1,9 +1,8 @@
+import { DeflateRaw } from 'zlib';
 import { getInput } from '../../getInput';
 import { BingoLine, BingoNumber, BingoSheet } from './Bingo';
 
 const calculateScore = () => {};
-
-const markNumber = () => {};
 
 const getBingoSheets = (array: string[]): BingoSheet[] => {
   let bingoSheets: BingoSheet[] = [];
@@ -28,16 +27,37 @@ const getBingoSheets = (array: string[]): BingoSheet[] => {
   return bingoSheets;
 };
 
+const markNumber = (draw: number[], sheets: BingoSheet[]): BingoLine[] => {
+  for (let i: number = 0; i < draw.length; i++) {
+    for (let n: number = 0; n < sheets.length; n++) {
+      let bingo: BingoLine[] = sheets[n].markNum(draw[i]);
+      if (bingo.length > 0) {
+        return bingo;
+      }
+    }
+  }
+  return [];
+};
+
 const bingoScore = () => {
   //getting data
   const data: string[] = getInput('input.txt', /\r?\n/);
   const [drawRaw, ...bingoBoardsRaw] = data;
   //splitting up data to draw numbers and bingo sheets
   const draw: string[] = drawRaw.split(',');
+  const drawNums: number[] = draw.map(value => {
+    return parseInt(value);
+  });
 
   //creating the sheets with classes
   const bingoBoards: any = bingoBoardsRaw.join(' ').split(' ');
   const sheets: BingoSheet[] = getBingoSheets(bingoBoards);
+
+  const bingo: BingoLine[] = markNumber(drawNums, sheets);
+
+  bingo.forEach(value => {
+    console.log(value);
+  });
   return 0;
 };
 
