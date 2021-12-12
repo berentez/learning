3d4a0c4e7d3504e2c3bd97226718b7c9710006a6
Zoltan Berente <berentez07b@gmail.com>
2021-12-12 16:15:03 +0100

    part one finished

--- advent-of-code/2021/06/fishCounter.ts
+++ advent-of-code/2021/06/fishCounter.ts
@@ -3,4 +3,23 @@ import { getInput } from '../../getInput';
 const testDays: number = 18;
 const days: number = 80;
 
-const fishCounter = (days: number) => {};
+const fishCounter = (days: number) => {
+  const data: string[] = getInput('input.txt', ',');
+  const fishData: number[] = data.map(value => parseInt(value));
+  let res: number = fishData.length;
+
+  for (let i: number = 0; i < days; i++) {
+    for (let n: number = 0; n < fishData.length; n++) {
+      if (fishData[n] === 0) {
+        fishData[n] = 7;
+        fishData.push(9);
+        res++;
+      }
+      fishData[n]--;
+    }
+  }
+  return res;
+};
+
+// console.log(fishCounter(testDays));
+console.log(fishCounter(days));
