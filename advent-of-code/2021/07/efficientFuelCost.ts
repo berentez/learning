1b79190a23f46e8143985ffb4258ab154f230131
Zoltan Berente <berentez07b@gmail.com>
2021-12-12 19:09:03 +0100

    test data working, input not

---
+++ advent-of-code/2021/07/efficientFuelCost.ts
@@ -0,0 +1,27 @@
+import { getInput } from '../../getInput';
+
+const efficientFuelCost = () => {
+  const data: string[] = getInput('input.txt', ',');
+  const crabsPosition: number[] = data.map(value => parseInt(value));
+
+  crabsPosition.sort((a, b) => a - b);
+
+  let fuel: number = Infinity;
+  let position: number = 0;
+
+  for (let i: number = 0; i < crabsPosition.length; i++) {
+    if (crabsPosition[i] !== crabsPosition[i + 1]) {
+      let fuelEstimation: number = 0;
+      for (let n: number = 0; n < crabsPosition.length; n++) {
+        fuelEstimation += Math.abs(crabsPosition[n] - crabsPosition[i]);
+      }
+      if (fuel > fuelEstimation) {
+        fuel = fuelEstimation;
+        position = crabsPosition[i];
+      }
+    }
+  }
+  return position;
+};
+
+console.log(efficientFuelCost());
