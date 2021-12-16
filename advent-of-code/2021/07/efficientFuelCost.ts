f5b4165664320910b2c8ce43fb632a782255b756
Zoltan Berente <berentez07b@gmail.com>
2021-12-16 19:29:19 +0100

    day 7 finished

--- advent-of-code/2021/07/efficientFuelCost.ts
+++ advent-of-code/2021/07/efficientFuelCost.ts
@@ -1,27 +1,58 @@
 import { getInput } from '../../getInput';
 
-const efficientFuelCost = () => {
-  const data: string[] = getInput('input.txt', ',');
-  const crabsPosition: number[] = data.map(value => parseInt(value));
+const data: string[] = getInput('input.txt', ',');
+const crabsPosition: number[] = data.map(value => parseInt(value));
 
-  crabsPosition.sort((a, b) => a - b);
+const efficientFuelCost = (data: number[]) => {
+  data.sort((a, b) => a - b);
 
   let fuel: number = Infinity;
   let position: number = 0;
 
-  for (let i: number = 0; i < crabsPosition.length; i++) {
-    if (crabsPosition[i] !== crabsPosition[i + 1]) {
+  for (let i: number = 0; i < data.length; i++) {
+    if (data[i] !== data[i + 1]) {
       let fuelEstimation: number = 0;
-      for (let n: number = 0; n < crabsPosition.length; n++) {
-        fuelEstimation += Math.abs(crabsPosition[n] - crabsPosition[i]);
+      for (let n: number = 0; n < data.length; n++) {
+        fuelEstimation += Math.abs(data[n] - data[i]);
       }
       if (fuel > fuelEstimation) {
         fuel = fuelEstimation;
-        position = crabsPosition[i];
+        position = data[i];
       }
     }
   }
   return fuel;
 };
 
-console.log(efficientFuelCost());
+const findHighest = (data: number[]) => {
+  let sort: number[] = data.sort((a, b) => a - b);
+
+  let res: number[] = [sort[0], sort[sort.length - 1]];
+  return res;
+};
+
+const calculateFuelCost = (data: number[]) => {
+  let values: number[] = findHighest(data);
+  let fuel: number = Infinity;
+  let n: number = 0;
+
+  for (let i: number = values[0]; i < values[1]; i++) {
+    let currentFuel: number = 0;
+    for (let j: number = 0; j < data.length; j++) {
+      if (i < data[j]) {
+        n = data[j] - i;
+      } else {
+        n = i - data[j];
+      }
+      let sum: number = (n * (n + 1)) / 2;
+      currentFuel += sum;
+    }
+    if (fuel > currentFuel) {
+      fuel = currentFuel;
+    }
+  }
+  return fuel;
+};
+
+console.log(efficientFuelCost(crabsPosition));
+console.log(calculateFuelCost(crabsPosition));
