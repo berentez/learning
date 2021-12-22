4326e3304caa54009cf94c4dc6b8c121ea532f3b
Zoltan Berente <berentez07b@gmail.com>
2021-12-22 15:56:56 +0100

    day 9 part 1 finished

--- advent-of-code/2021/09/smoke-basin.ts
+++ advent-of-code/2021/09/smoke-basin.ts
@@ -1,11 +1,40 @@
 import { getInput } from '../../getInput';
 
-const rawData: string[] = getInput('testInput.txt', /\r?\n/);
+const rawData: string[] = getInput('input.txt', /\r?\n/);
 const data: number[][] = rawData.map(value =>
   value.split('').map(n => parseInt(n))
 );
 
-const lowPoints = (data: string[]) => {
-  for (let i: number = 0; i < data.length; i++) {}
+const borderData = (data: number[][]) => {
+  let line: number[] = [];
+  for (let i: number = 0; i < data[0].length + 2; i++) {
+    line.push(9);
+  }
+
+  for (let i: number = 0; i < data.length; i++) {
+    data[i].unshift(9);
+    data[i].push(9);
+  }
+  data.unshift(line);
+  data.push(line);
+  return data;
 };
-console.log(lowPoints);
+
+const lowPointsRisk = (data: number[][]) => {
+  borderData(data);
+  let lowPoints: number[] = [];
+  for (let i: number = 1; i < data.length; i++) {
+    for (let n: number = 1; n < data[i].length; n++) {
+      if (
+        data[i][n] < data[i][n - 1] &&
+        data[i][n] < data[i][n + 1] &&
+        data[i][n] < data[i - 1][n] &&
+        data[i][n] < data[i + 1][n]
+      ) {
+        lowPoints.push(data[i][n] + 1);
+      }
+    }
+  }
+  return lowPoints.reduce((a, b) => a + b);
+};
+console.log(lowPointsRisk(data));
