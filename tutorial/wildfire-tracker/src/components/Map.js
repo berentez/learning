77d7719b8ca80efa815d4c91c36775d018666b50
Zoltan Berente <berentez07b@gmail.com>
2022-01-28 17:50:09 +0100

    googlemaps added

--- tutorial/wildfire-tracker/src/components/Map.js
+++ tutorial/wildfire-tracker/src/components/Map.js
@@ -4,11 +4,20 @@ const Map = ({ center, zoom }) => {
   return (
     <div className="map">
       <GoogleMapReact
-        //this should be in a env file
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}
       ></GoogleMapReact>
     </div>
   );
+};
+
+Map.defaultProps = {
+  center: {
+    lat: 42.3265,
+    lng: -122.8756,
+  },
+  zoom: 6,
 };
 
 export default Map;
