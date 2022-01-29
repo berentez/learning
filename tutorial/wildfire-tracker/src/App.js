bc149c9f425f3984e90f7d3bad63cc8049d75b2a
Zoltan Berente <berentez07b@gmail.com>
2022-01-29 17:21:09 +0100

    tutorial finished

--- tutorial/wildfire-tracker/src/App.js
+++ tutorial/wildfire-tracker/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Map from './components/Map';
 import Loader from './components/Loader';
+import Header from './components/Header';
 
 function App() {
   const [eventData, setEventData] = useState([]);
@@ -23,7 +24,12 @@ function App() {
     console.log(eventData);
   }, []);
 
-  return <div>{!loading ? <Map eventData={eventData} /> : <Loader />} </div>;
+  return (
+    <div>
+      <Header />
+      {!loading ? <Map eventData={eventData} /> : <Loader />}{' '}
+    </div>
+  );
 }
 
 export default App;
