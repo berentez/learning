8c04aac3daf9c2fa64bea778578e06a50a4befc4
Zoltan Berente <berentez07b@gmail.com>
2022-02-25 15:34:53 +0100

    finishing projects up

--- tutorial/portfolio_website/src/components/Hero/Hero.js
+++ tutorial/portfolio_website/src/components/Hero/Hero.js
@@ -4,10 +4,27 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        Zoltan Berente's Portfolio
+      </SectionTitle>
+      <SectionText>
+        This is a next.js tutorial project from JavaScript Mastery. The first time I'm using
+        next.js, also getting more familiar with styled components.
+      </SectionText>
+      <Button
+        onClick={() =>
+          (window.location =
+            'https://www.youtube.com/watch?v=OPaLnMw2i_0&ab_channel=JavaScriptMastery')
+        }
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
