8e6ee0acf309d0f414e0459d57aa02e3c4e6b06a
Zoltan Berente <berentez07b@gmail.com>
2022-03-04 15:04:05 +0100

    reference types

--- java/tutorial/HelloWorld/src/com/berentez/Main.java
+++ java/tutorial/HelloWorld/src/com/berentez/Main.java
@@ -1,13 +1,14 @@
 package com.berentez;
 
+
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-
-        char letter = 'A';
-        boolean isEligible = true;
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
