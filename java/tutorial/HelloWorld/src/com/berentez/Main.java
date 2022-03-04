4f697423cb4e0f09cb3b32f39e480651ca24cd07
Zoltan Berente <berentez07b@gmail.com>
2022-03-04 14:51:58 +0100

    practicing java types

--- java/tutorial/HelloWorld/src/com/berentez/Main.java
+++ java/tutorial/HelloWorld/src/com/berentez/Main.java
@@ -3,6 +3,11 @@ package com.berentez;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+
+        char letter = 'A';
+        boolean isEligible = true;
     }
 }
