f1054651a41b282ceb070b285963c834441962c7
Zoltan Berente <berentez07b@gmail.com>
2022-03-05 18:11:34 +0100

    for each loop in java

--- java/tutorial/conditionals/src/com/berentez/Main.java
+++ java/tutorial/conditionals/src/com/berentez/Main.java
@@ -6,12 +6,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
-        while (!input.equals("quit")) {
-            System.out.print("Input: ");
-            input = scanner.next().toLowerCase();
-            System.out.println(input);
-        }
+        String[] fruits = {"Apple", "Mango", "Orange"};
+
+        for (String fruit : fruits)
+            System.out.println(fruit);
+
     }
 }
