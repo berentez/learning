04e570d401079373dca76a1ed33e832aa27b4955
Zoltan Berente <berentez07b@gmail.com>
2022-03-05 18:04:14 +0100

    while loop in java

--- java/tutorial/conditionals/src/com/berentez/Main.java
+++ java/tutorial/conditionals/src/com/berentez/Main.java
@@ -1,16 +1,17 @@
 package com.berentez;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-    int temp = 10;
-    if (temp > 30) {
-        System.out.println("It's a hot day");
-        System.out.println("Drink water");
-    }
-    else if (temp > 20 && temp <= 30)
-        System.out.println("beautiful day");
-    else
-        System.out.println("cold day");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
     }
 }
